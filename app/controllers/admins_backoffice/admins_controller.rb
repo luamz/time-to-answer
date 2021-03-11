8d63d8978408e1f0f095c3b1cc7cd475c5bc423b
luamz <luamsilva@id.uff.br>
2021-03-10 22:29:06 -0300

    Admins_backoffice finalizada

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,25 +1,56 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password, only: [:update] 
+  before_action :set_admin, only: [:edit, :update, :destroy]
+
   def index
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
   def edit
-    @admin = Admin.find(params[:id])
   end
 
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      #Caso a modificação seja só no email
-      params[:admin].extract!(:password,:password_confirmation)
-    end
-    
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-    
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
     end
+  end
+
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Administrador excluído com sucesso!"
+    else
+      render :index
+    end
+  end
+
+  private
+  def verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      #Caso a modificação seja só no email
+      params[:admin].extract!(:password,:password_confirmation)
+    end
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
   end
 end
