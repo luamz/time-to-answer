993bfc9f157d3f6b0c0ee0facdf983436adae479
luamz <luamsilva@id.uff.br>
2021-03-14 20:54:23 -0300

    Edit do users_backoffice finalizado

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -1,34 +1,34 @@
 class UsersBackoffice::ProfileController < UsersBackofficeController
-  before_action :verify_password, only: [:update] 
+  before_action :verify_password, only: [:update]
   before_action :set_user
-
-
+  
   def edit
+    @user.build_user_profile if @user.user_profile.blank?
   end
 
   def update
     if @user.update(params_user)
-      sign_in(@user, bypass:true)
+      bypass_sign_in(@user)
       redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
     else
       render :edit
-    end
+    end    
   end
 
   private
-  def verify_password
-    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
-      #Caso a modificação seja só no email
-      params[:user].extract!(:password,:password_confirmation)
-    end
-  end
 
   def set_user
     @user = User.find(current_user.id)
   end
 
   def params_user
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
+    user_profile_attributes: [:id,:adress, :gender, :birthdate])
   end
 
+  def verify_password
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].extract!(:password, :password_confirmation)
+    end
+  end
 end
