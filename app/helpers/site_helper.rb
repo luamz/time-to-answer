6852092a4eb17dcd4b06e5f298e5faf7ccf4b70a
luamz <luamsilva@id.uff.br>
2021-03-14 14:44:14 -0300

    Tags de Assunto

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,13 @@
 module SiteHelper
+
+    def msg_jumbotron
+        case params[:action]
+        when 'index'
+            "Últimas questões cadastradas..."
+        when 'questions'
+            "Resultados para \"#{params[:term]}\"..."
+        when 'subject'
+            "Mostrando questões do assunto \"#{params[:subject]}\""
+            end
+    end
 end
