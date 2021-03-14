8e49956ded9aed065b8d1e5f33f77cac50397d49
luamz <luamsilva@id.uff.br>
2021-03-13 21:34:06 -0300

    Implementando mecanismo de pesquisa

---
+++ app/controllers/site/search_controller.rb
@@ -0,0 +1,6 @@
+class Site::SearchController < SiteController
+    def questions
+        @questions = Question.search(params[:page], params[:term])
+    end
+end
+
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,19 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  #Kaminari
+  paginates_per 5
+
+  def self.search(page, term) #Método Estático
+    Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page)
+  end
+
+  def self.last_questions(page)
+    Question.includes(:answers).order('created_at desc').page(page)
+  end
+
+
 end
