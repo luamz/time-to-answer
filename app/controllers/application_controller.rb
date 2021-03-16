5463fb02a3f2e6e4dc3d7d0a80daf78fe630c656
luamz <luamsilva@id.uff.br>
2021-03-16 18:08:21 -0300

    Projeto concluido

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     layout :layout_by_resource
     before_action :check_pagination
+    before_action :set_global_params
 
     protected
     def layout_by_resource
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
             params.extract!(:page)
         end
     end
+
+    def set_global_params
+        $global_params = params
+    end
+
 end
